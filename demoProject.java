c551ac4c7db4ea2b098551bced030513b9d27cd4
Sharn03 <147249297+Sharn03@users.noreply.github.com>
2024-04-06 20:35:05 +0530

    1

--- demoProject.java
+++ demoProject.java
@@ -8,10 +8,10 @@ public class demoProject {
 				Class.forName("com.mysql.jdbc.Driver");
 				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guvi_tasks","root","root");
 				Statement st = con.createStatement();
-				ResultSet rs = st.executeQuery("Select * from empl");
+				ResultSet rs = st.executeQuery("Select * from employee");
 				while(rs.next())
 				{
-					System.out.println(rs.getString(1)+ " " + rs.getString(2));
+					System.out.println(rs.getString(1)+ " " + rs.getString(2) +" "+rs.getString(3)+ " "+ rs.getString(4));
 				}
 			}
 			catch(Exception e)
